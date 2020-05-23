0f5bfc1e9602b6d7b23cf927fa807306820d50b0
Nkunzi <nkunziinnocent@gmail.com>
2020-05-24 00:36:38 +0200

    created action creators and api.js

---
+++ src/actions/shared.js
@@ -0,0 +1,16 @@
+import {getInitialData} from '../utils/api'
+import { getQuestions } from './questions'
+import {getUsers} from './users'
+import {setAuthedUser} from './authedUser'
+
+const AUTHED_USER = null
+
+export function handleInitialQuestions(){
+    return (dispatch) => {
+        return getInitialData().then(({users, questions}) => {
+            dispatch(getUsers(users))
+            dispatch(getQuestions(questions))
+            dispatch(setAuthedUser(AUTHED_USER))
+        })
+    }
+}
